5963a18eb557934b68159668251196be06738849
Lincoln Hach Yellick <lincoln.yellick@kerberus.io>
2026-02-24 18:36:07 -0600

    upgraded platformio and started messing around with controlling the beluga

--- LittleBeluga/src/littlebeluga.ino
+++ LittleBeluga/src/littlebeluga.ino
@@ -1,5 +1,4 @@
 #include <Arduboy2.h>
-#include <Sprites.h>
 #include <ArduboyTones.h>
 
 #include "sound.h"
@@ -7,13 +6,16 @@
 
 Arduboy2 arduboy;
 ArduboyTones sound(arduboy.audio.enabled);
-Sprites sprites;
-
-
+constexpr uint8_t BELUGA_WIDTH = 15;
+constexpr uint8_t BELUGA_HEIGHT = 15;
+int16_t belugaX = (WIDTH - BELUGA_WIDTH) / 2;
+int16_t belugaY = (HEIGHT - BELUGA_HEIGHT) / 2;
+bool inTitleScreen = true;
 
 void setup() {
   arduboy.begin();
   arduboy.setFrameRate(60);
+  sound.tones(titleScreenScore);
 }
 
 void loop() {
@@ -23,9 +25,38 @@ void loop() {
 
   arduboy.pollButtons();
 
-  uint8_t button = arduboy.buttonsState();
+  if (inTitleScreen) {
+    if (arduboy.buttonsState() != 0) {
+      inTitleScreen = false;
+      sound.noTone();
+    }
 
-  sprites.drawSelfMasked(0, 0, titleScreen, 60);
+    arduboy.drawBitmap(0, 0, titleScreen, WIDTH, HEIGHT, WHITE);
+    arduboy.display(CLEAR_BUFFER);
+    return;
+  }
+
+  if (arduboy.pressed(LEFT_BUTTON) && belugaX > 0) {
+    --belugaX;
+  }
+  if (arduboy.pressed(RIGHT_BUTTON) && belugaX < WIDTH - BELUGA_WIDTH) {
+    ++belugaX;
+  }
+  if (arduboy.pressed(UP_BUTTON) && belugaY > 0) {
+    --belugaY;
+  }
+  if (arduboy.pressed(DOWN_BUTTON) && belugaY < HEIGHT - BELUGA_HEIGHT) {
+    ++belugaY;
+  }
+
+  if (arduboy.justPressed(A_BUTTON)) {
+    sound.tones(bubblePop);
+  }
+  if (arduboy.justPressed(B_BUTTON)) {
+    sound.tones(bubbleGenerate);
+  }
+
+  arduboy.drawBitmap(belugaX, belugaY, beluga + 2, BELUGA_WIDTH, BELUGA_HEIGHT, WHITE);
 
   arduboy.display(CLEAR_BUFFER);
 }
